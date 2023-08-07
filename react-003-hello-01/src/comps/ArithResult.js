c15ff5c9b8c0ca10f44498eb687183c4776d9f3d
cccff12 <ksun0430@naver.com>
2023-08-07 11:50:28 +0900

    8.7오전

--- react-003-hello-01/src/comps/ArithResult.js
+++ react-003-hello-01/src/comps/ArithResult.js
@@ -5,6 +5,8 @@ const Arithresult = ({ nums }) => {
 
   // const intNum1= Number(num1)
   // const intNum2= Number(num2)
+
+  //   input에 입력한 값을 숫자로 바꿔주는 코드
   const [intNum1, intNum2] = [Number(num1), Number(num2)];
   //   아래9줄을 20열의 한 줄로 대체: 최대값과 최소값을 정렬
   //   let minNum = 0;
@@ -18,6 +20,11 @@ const Arithresult = ({ nums }) => {
   //   }
 
   const [minNum, maxNum] = num2 > num1 ? [num1, num2] : [num2, num1];
+
+  // 여기까지 rendering이 시작되기전에 실행하는 코드
+  // nums State가 변화되면 ArithResult 함수가 실행
+
+  //여기부터rendering이 시작되는 부분
 
   return (
     <div>
